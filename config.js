6cb9e32bdf37cbe25878df80be4190850dc0fa5b
ashokdhar <soibaal@yahoo.com>
2020-05-27 19:45:02 +0530

    changed app

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'testapp',
+		wp_ws_url : 'https://ashokdhar.in/sidentertainment/wp-appkit-api/testapp',
+		wp_url : 'https://ashokdhar.in/sidentertainment',
+		theme : 'q-android',
+		version : '10',
+		app_type : 'phonegap-build',
+		app_title : 'Testapp',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'u;/;F^br5%2V%HpdhMh-k9De!].u PN*#ogd+s_EP:C?[)Fq><lJ-eL=k=tGRc94',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
